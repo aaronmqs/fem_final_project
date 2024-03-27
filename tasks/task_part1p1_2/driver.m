6305c18083575f6182619bb8677d1262c187ba94
Aaron Marques <rmarque2@nd.edu>
2024-03-27 11:09:43 -0400

    Item 2) of Tasks for Part 1.1.

---
+++ tasks/task_part1p1_2/driver.m
@@ -0,0 +1,26 @@
+clear; clc; close all;
+
+d = 2; p = 2;
+[zk, f2v, N] = create_nodes_bndy_refdom_simp(d, p);
+
+nf = size(f2v, 2);
+mrkrs = {'x'; 'square'; '+'};
+clrs = ['r', 'g', 'k'];
+for i = 1:nf
+    figure
+    plot(zeros(100, 1), linspace(0, 1, 100), 'k')
+    hold on
+    plot(linspace(0, 1, 100), zeros(100, 1), 'k')
+    hold on
+    plot(linspace(0, 1, 1000), 1 - linspace(0, 1, 1000), 'k')
+    hold on
+    scatter(zk(1,:), zk(2,:), 'filled', 'MarkerFaceColor', 'b')
+    hold on
+    nds_fc = f2v(:, i);
+    scatter(zk(1, nds_fc), zk(2, nds_fc), clrs(i), 'Marker', mrkrs{i}, 'LineWidth', 2, 'SizeData', 300)
+    title('Nodes on face', i)
+    grid on
+    axis([-0.1 1.1 -0.1 1.1])
+
+    fname = sprintf('C:/Users/aaron/PhD/2024_1_AME60541/project/fem_final_project/tasks/Part1p1/face%d', i); saveas(gcf, [fname, '.png']); close all;
+end
