4801954bea78bcc2f71a68e5f58af0ee2a67306a
Aaron Marques <rmarque2@nd.edu>
2024-03-29 19:46:03 -0400

    Simplified init.m

--- init.m
+++ init.m
@@ -1,10 +1,12 @@
-addpath('_eqn/');
-addpath('_fem/');
-addpath('_mesh/');
-addpath('_mesh/_distmesh/');
-addpath('_mesh/_meshes');
-addpath('_util/');
-addpath('_wrappers/');
+addpath(genpath('..\fem_final_project\'));
 
-% Other folders
-addpath(genpath('tasks/'));
+% addpath('_eqn/');
+% addpath('_fem/');
+% addpath('_mesh/');
+% addpath('_mesh/_distmesh/');
+% addpath('_mesh/_meshes');
+% addpath('_util/');
+% addpath('_wrappers/');
+% 
+% % Other folders
+% addpath(genpath('tasks/'));
