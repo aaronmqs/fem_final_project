ea2d8305d6db6e013aa66be832af2b289bc1c7d2
Aaron Marques <rmarque2@nd.edu>
2024-04-03 14:05:35 -0400

    Task 3 for Part 1.4.

--- tasks/task_part1p4_3/chk_sphere_domain.m
+++ tasks/task_part1p4_3/chk_sphere_domain.m
@@ -1,4 +1,4 @@
-function [varargout] = chk_sphere_domain(etype, ndim, nel, porder)
+function [varargout] = chk_sphere_domain(etype, ndim, nel, porder, pltit)
 % This function checks if the volume, the centroid and the surface area of
 % a ndim-dimensional sphere domain which mesh is formed by hypercube elements is
 % evaluated correctly.
@@ -10,12 +10,14 @@ c = zeros(ndim, 1); r = 1;
 msh = create_mesh_hsphere(etype, c, r, nel, porder);
 
 % Visualize mesh
-if ndim == 1 || ndim == 2
-    visualize_fem([], msh);
-elseif ndim == 3
-    visualize_fem3d([], msh);
-else
-    warning("Visualization not implemented.")
+if pltit
+    if ndim == 1 || ndim == 2
+        visualize_fem([], msh);
+    elseif ndim == 3
+        visualize_fem3d([], msh);
+    else
+        warning("Visualization not implemented.")
+    end
 end
 
 % Compute metrics (volume, centroid, surface area)
@@ -29,13 +31,13 @@ transf_data = create_transf_data_ndim(lfcnsp, msh.xcg, msh.e2vcg, msh.e2bnd);
 if nargout > 0
     if ndim == 2
         ev = abs(v - pi * r^2);
-        esa = abs(sa - 4 * pi * r^2);
+        esa = abs(sa - 2 * pi * r);
     else
         ev = abs(v - 4 * pi * r^3 / 3);
         esa = abs(sa - 4 * pi * r^2);
     end
     ec = norm(c);
-    varargout = [ev, esa, ec];
+    varargout = {ev, esa, ec};
 else
     tol = 1e-8;
     if ndim == 2
@@ -47,10 +49,9 @@ else
     end    
 
     if ~isempty(find(abs(c) > tol, 1)); error("Incorrect centroid."); end
+
+    % If this point is reached, the test was successful.
+    fprintf("Test for hypercube passed.\n\n")
 end
-
-
-% If this point is reached, the test was successful.
-fprintf("Test for hypercube passed.\n\n")
 
 end
--- tasks/task_part1p4_3/driver.m
+++ tasks/task_part1p4_3/driver.m
@@ -1,8 +1,31 @@
 clear; clc; close all;
 
-% hcube
-for ndim = 2:3
-    for porder = 1:4
-        chk_sphere_domain_hcube(ndim, nel, porder);
+bffr = zeros(2, 2, 4);
+[ev, esa, ec] = deal(bffr, bffr, bffr);
+i = 0;
+tol = 1e-6; pltit = 0;
+for etype = {'hcube', 'simp'}
+    i = i + 1;
+    for ndim = 2:3
+        for porder = 1:4
+            nel_base = 2^ndim * ones(ndim, 1);
+            for j = 1:2
+                nel = j * nel_base;
+                if ~(strcmp(etype{:}, 'simp') && ndim == 3)
+                    fprintf("\nTest for %d-dimensional mesh of %d %s elements (per dimension) of order %d: \n\n", ndim, i * nel(1), etype{:}, porder);
+                    [ev(i, ndim, porder, j), esa(i, ndim, porder, j), ec(i, ndim, porder, j)] = chk_sphere_domain(etype{:}, ndim, nel, porder, pltit);
+                    fprintf("Volume error: %d. \n", ev(i, ndim, porder, j))
+                    fprintf("Surface area error: %d. \n", esa(i, ndim, porder, j))
+                    fprintf("Centroid error: %d. \n", ec(i, ndim, porder, j))
+                    if norm([ev(i, ndim, porder, j), esa(i, ndim, porder, j), ec(i, ndim, porder, j)], "inf") < tol
+                        fprintf("\nThis number of elements/polynomial order are enough to evaluate the integrals for this mesh. \n")
+                    end
+                end
+            end
+        end
     end
 end
+
+save("ev", "ev")
+save("esa", "esa")
+save("ec", "ec")
