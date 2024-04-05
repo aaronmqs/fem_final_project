8e5470b5827214696d56c4820ae0660cb6bb26ad
Aaron Marques <rmarque2@nd.edu>
2024-04-05 13:47:34 -0400

    Updated checks for Lagrangian properties.

--- tasks/task_part1p2_3/check_lagrangian_properties.m
+++ tasks/task_part1p2_3/check_lagrangian_properties.m
@@ -10,11 +10,16 @@ function check_lagrangian_properties(ndim, porder, x)
 %
 % check_lagrangian_properties(3, 5, [0.2 0.3; 0 0; 0.1 0.1])
 
-xk = create_nodes_bndy_refdom_simp(ndim, porder); 
+xk = create_nodes_bndy_refdom_simp(ndim, porder);
+nv = size(xk, 2);
 if size(x, 1) ~= size(xk, 1); error("Incorrect dimension for evaluation points."); end
 Q = eval_interp_simp_lagrange(xk, x);
+
+% Checks
 nx = size(x, 2);
 tol = 1e-8;
+Qk = eval_interp_simp_lagrange(xk, xk);
+if norm(squeeze(Qk(:, 1, :)) - eye(nv), "inf") > tol; error("Lagrangian (nodal) property not satisfied."); end
 if ~isempty(setdiff(find(abs(sum(squeeze(Q(:, 1, :)), 1) - 1) > tol), 1:nx))
     error("The sum of all basis functions should be equal to 1 for any given point.")
 end
