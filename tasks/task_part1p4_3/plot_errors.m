ea4b4a883e93028e68b4f9b4f08c0a36fa571f9e
Aaron Marques <rmarque2@nd.edu>
2024-04-06 16:19:09 -0400

    Error analysis.

--- tasks/task_part1p4_3/plot_errors.m
+++ tasks/task_part1p4_3/plot_errors.m
@@ -11,26 +11,61 @@ for ndim = 2:3
         if ~(i == 2 && ndim == 3)
             figure
             for porder = 4:4
-                idx = 4:length(nelem_per_dim);
+                idx = 1:length(nelem_per_dim);
                 subplot(3, 1, 1)
                 hold on;
-                semilogy(nelem_per_dim(idx), squeeze(ec(i, ndimm1, porder, idx))); grid on;
+                loglog(nelem_per_dim(idx), squeeze(ec(i, ndimm1, porder, idx)), 'Marker','o'); grid on; 
                 ylabel("Centroid error")
-                legend("p1", "p2", "p3", "p4")
+                xticks(nelem_per_dim)
                 subplot(3, 1, 2)
                 hold on;
-                semilogy(nelem_per_dim(idx), squeeze(esa(i, ndimm1, porder, idx))); grid on;
+                loglog(nelem_per_dim(idx), squeeze(esa(i, ndimm1, porder, idx)), 'Marker', 'diamond'); grid on;
                 ylabel("Surf. area error")
-                legend("p1", "p2", "p3", "p4")
+                xticks(nelem_per_dim)
                 subplot(3, 1, 3)
                 hold on;
-                semilogy(nelem_per_dim(idx), squeeze(ev(i, ndimm1, porder, idx))); grid on;
+                semilogy(nelem_per_dim(idx), squeeze(ev(i, ndimm1, porder, idx)), 'Marker', 'square'); grid on;
                 ylabel("Volume error")
-                legend("p1", "p2", "p3", "p4")
                 xlabel("Number of elements");
+                xticks(nelem_per_dim)
             end
-            str = sprintf("%d-dimensional %s element.", ndim, etype{i});
+            str = sprintf("%d-dimensional %s element (p = 4).", ndim, etype{i});
             sgtitle(str);
+            fname = sprintf('C:/Users/aaron/PhD/2024_1_AME60541/project/fem_final_project/tasks/task_part1p4_3/nel_ndim%g_%s', ndim, etype{i});
+            saveas(gcf, [fname, '.png']); close all;
         end
     end
 end
+
+for ndim = 2:3
+    ndimm1 = ndim - 1;
+    nelem_per_dim = 1:2:2^(3 + 1);
+    for i = 1:2 % 1=hcube, 2=simp
+        if ~(i == 2 && ndim == 3)
+            figure
+            for idx = length(nelem_per_dim):length(nelem_per_dim)
+                subplot(3, 1, 1)
+                hold on;
+                loglog(1:4, squeeze(ec(i, ndimm1, :, idx)), 'Marker','o'); grid on; 
+                ylabel("Centroid error");
+                xticks([1 2 3 4])
+                subplot(3, 1, 2)
+                hold on;
+                loglog(1:4, squeeze(esa(i, ndimm1, :, idx)), 'Marker', 'diamond'); grid on;
+                ylabel("Surf. area error");
+                xticks([1 2 3 4])
+                subplot(3, 1, 3)
+                hold on;
+                semilogy(1:4, squeeze(ev(i, ndimm1, :, idx)), 'Marker', 'square'); grid on;
+                ylabel("Volume error")
+                xlabel("Polynomial degree");
+                xticks([1 2 3 4])
+            end
+            str = sprintf("%d-dimensional %s element (nelem = %d).", ndim, etype{i}, nelem_per_dim(end));
+            sgtitle(str);
+            fname = sprintf('C:/Users/aaron/PhD/2024_1_AME60541/project/fem_final_project/tasks/task_part1p4_3/p_ndim%g_%s', ndim, etype{i});
+            saveas(gcf, [fname, '.png']); close all;
+        end
+    end
+end
+
