014a044dba8afdc9fedd75c7ff85e0792a5c227e
Aaron Marques <rmarque2@nd.edu>
2024-04-27 21:57:10 -0400

    part 4.6

--- tasks/task_part4p6/driver.m
+++ tasks/task_part4p6/driver.m
@@ -1,15 +1,24 @@
 clear; clc; close all;
 
-cnt = 0; e = zeros(4, 5);
+kmax = 4;
+pmax = 5;
+ocr = zeros(pmax, 1);
+e = zeros(4, 5);
 for porder = 1:5
-    for k = 1:4
-        nel = 2^k; pltit = false; cnt = cnt + 1;
+    for k = 1:kmax
+        nel = 2^k; pltit = false;
         [U, e(k, porder), info] = solve_pde0(nel, porder, pltit);
     end
-    h = 1 ./ (porder * 2.^(1:4));
-    ocr = log(e(1, porder) / e(end, porder)) / log(h(1) / h(end));
-    subplot(5, 1, porder)
-%     figure
-    loglog(h, e(:, porder), 'Marker','square'); grid on;
-    title("p = " + num2str(porder)+ ", Opt. conv. rate = " + num2str(ocr))
+    h = 1 ./ (2.^(1:kmax));
+    if porder < 5
+        ocr(porder) = log(e(1, porder) / e(end, porder)) / log(h(1) / h(end));
+    else
+        ocr(porder) = log(e(1, porder) / e(end-1, porder)) / log(h(1) / h(end-1));
+    end
+%     subplot(5, 1, porder)
+    loglog(h, e(:, porder), 'Marker','square'); grid on; hold on;
+%     title("p = " + num2str(porder)+ ", Opt. conv. rate = " + num2str(ocr(porder)))
+    fprintf("p = " + num2str(porder)+ ", Opt. conv. rate = " + num2str(ocr(porder)) + "\n\n")
 end
+
+legend("p1", "p2", "p3", "p4", "p5")
